1da3a372f28113da379c3670a6c72e57597914fe
avuign <ant.vuign@gmail.com>
2026-03-24 11:46:11 +0100

    added training loop

---
+++ train.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+def train(model, X, Y, num_epochs, batch_size, lr):
+
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+
+    for epoch in range(num_epochs):
+        for i in range(0, len(X), batch_size):
+            batch = {"chars": X[i : i + batch_size], "next_char": Y[i : i + batch_size]}
+
+            logits = model.forward(torch.tensor(batch["chars"]))
+            loss = nn.CrossEntropyLoss()(logits, torch.tensor(batch["next_char"]))
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        print(f"Epoch {epoch + 1}, Loss: {loss}")
